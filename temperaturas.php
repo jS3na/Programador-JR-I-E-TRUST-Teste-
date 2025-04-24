00da8d260111f2f7b03cc637f42f31ef6d9c72db
js3na <passosjoaogabriel29@gmail.com>
2025-04-24 00:30:35 -0300

    feat: verificação da temperatura mais próxima de 0

--- temperaturas.php
+++ temperaturas.php
@@ -7,8 +7,29 @@
 * @return int A temperatura mais proxima de zero
 **/
 function menorTemperatura($temperaturas) {
+
 	
-	return(0);
+	$maisProximoZero = null; // inicializa o maisProximoZero como null, pois pode ser que o mais proximo de 0 seja o proprio 0
+	
+	foreach($temperaturas as $temperatura){ // loop que percorre o array
+
+		if($maisProximoZero === null){ // se for a primeira iteração, ele define o maisProximoZero como o primeiro numero do array
+			$maisProximoZero = $temperatura;
+		}
+
+		// essas duas linhas fazem a verificação de que se a temperatura for positiva, usa ela mesmo, e se não, transforma em positiva
+		$difAtual = $temperatura > 0 ? $temperatura : ($temperatura * -1); // diferença de 0 da temperatura atual
+		$difMenor = $maisProximoZero > 0 ? $maisProximoZero : ($maisProximoZero * -1); // diferença de 0 da temperatura que foi definida como menor até então
+
+		if($difAtual < $difMenor){ // verifica se a diferença da temperatura atual for menor que a diferença da temperatura definida
+			$maisProximoZero = $temperatura;
+		}
+		else if($difAtual === $difMenor && $temperatura > $maisProximoZero){ // verifica se as distâncias são iguais e coloca como prioridade a positiva
+			$maisProximoZero = $temperatura;
+		}
+	}
+
+	return $maisProximoZero;
 }
 
 
