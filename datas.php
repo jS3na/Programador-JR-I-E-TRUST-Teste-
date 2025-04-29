8bbb0c6a179fbfc32ad528fa9220eb3275f0c46b
js3na <passosjoaogabriel29@gmail.com>
2025-04-29 18:23:55 -0300

    refactor: removendo orientação a objetos da main

--- datas.php
+++ datas.php
@@ -1,9 +1,5 @@
 <?php
 
-require 'vendor/autoload.php';
-
-use Classes\CalculadoraData;
-
 /**
  * Calcula o numero de dias entre 2 datas.
  * As datas passadas sempre serao validas e a primeira data sempre sera menor que a segunda data.
@@ -19,146 +15,237 @@ use Classes\CalculadoraData;
  * calcular os dias do ultimo ano
  */
 
-$calculadora = new CalculadoraData();
+//função para verificar se o ano é bissexto
+function verificaBissexto($ano): bool
+{
+    return (($ano % 4 == 0 && $ano % 100 != 0) || ($ano % 400 == 0));
+}
+
+//gera o modelo de dias com base se o ano é bissexto ou não
+function geraModeloDias($bissexto): array
+{
+    return [
+        1 => 31,
+        2 => $bissexto ? 29 : 28,
+        3 => 31,
+        4 => 30,
+        5 => 31,
+        6 => 30,
+        7 => 31,
+        8 => 31,
+        9 => 30,
+        10 => 31,
+        11 => 30,
+        12 => 31
+    ];
+}
+
+//gera os dias do ano com base se ele é bissexto ou não
+function geraDiasBissexto($bissexto): int
+{
+    return $bissexto ? 366 : 365;
+}
+
+function calculaDias($dataInicial, $dataFinal)
+{
+    /*
+        - Setembro, abril, junho e novembro tem 30 dias, todos os outros meses tem 31 exceto fevereiro que tem 28, exceto nos anos bissextos nos quais ele tem 29.
+        - Os anos bissexto tem 366 dias e os demais 365.
+        - Todo ano divisivel por 4 e um ano bissexto.
+        - A regra acima não e valida para anos divisiveis por 100. Estes anos devem ser divisiveis por 400 para serem anos bissextos. Assim, o ano 1700, 1800, 1900 e 2100 nao sao bissextos, mas 2000 e bissexto.
+        - Não e permitido o uso de classes e funcoes de data da linguagem (DateTime, mktime, strtotime, etc).
+    */
+
+    list($anoInicial, $mesInicial, $diaInicial) = array_map('intval', explode("-", $dataInicial)); //variaveis da data inicial (em inteiros)
+    list($anoFinal, $mesFinal, $diaFinal) = array_map('intval', explode("-", $dataFinal)); //variaveis da data final (em inteiros)
+
+    $diferencaAnos = $anoFinal - $anoInicial;
+
+	$modeloDiasAno = geraModeloDias(verificaBissexto($anoInicial)); //gera o modelo de dias com base no ano inicial
+
+    if ($diferencaAnos > 0) { //verifica se as datas iniciais e finais são de anos diferentes
+
+        /**
+         * CASO AS DATAS FOREM DE ANOS DIFERENTES
+         */
+
+        $diasTotais = geraModeloDias(verificaBissexto($anoInicial))[$mesInicial] - $diaInicial; //calcula os dias restantes do primeiro mes
+
+        for ($i = $mesInicial + 1; $i <= 12; $i++) { //loop que percorre os meses até o fim do ano inicial
+            $diasTotais += $modeloDiasAno[$i]; //resulta na quantidade de dias do ano inicial
+        }
+
+        for ($j = 1; $j < $diferencaAnos; $j++) { //loop que percorre os anos entre as duas datas e calcula seus dias
+            $anoInicial++;
+            $diasAnoAtual = geraDiasBissexto(verificaBissexto($anoInicial));
+            $diasTotais += $diasAnoAtual;
+        }
+
+        $modeloDiasAnoFinal = geraModeloDias(verificaBissexto($anoFinal)); //gera o modelo dos dias do ultimo ano
+
+        for ($i = 1; $i < $mesFinal; $i++) { //lopp que percorre os meses do ultimo ano
+            $diasTotais += $modeloDiasAnoFinal[$i]; //soma dos dias do ultimo ano
+        }
+    } else {
+
+        /**
+         * CASO AS DATAS FOREM DE ANOS IGUAIS
+         */
+
+        if ($mesInicial === $mesFinal) { // verifica se são do mesmo mes
+            return $diaFinal - $diaInicial; // subtrai os dias
+        }
+
+        $diasTotais = $modeloDiasAno[$mesInicial] - $diaInicial; // calcula os dias do primeiro mes
+
+        for ($k = $mesInicial + 1; $k < $mesFinal; $k++) { // loop que percorre os meses no intervalo entre o inicial e o final
+            $diasTotais += $modeloDiasAno[$k]; // soma os dias com base no modelo
+        }
+    }
+
+    $diasTotais += $diaFinal; // soma dos ultimos dias, do ultimo mês, do último ano
+
+    return $diasTotais;
+}
 
 /***** Teste 01 *****/
 $dataInicial = "2018-01-01";
 $dataFinal = "2018-01-02";
 $resultadoEsperado = 1;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("01", $resultadoEsperado, $resultado);
 
 /***** Teste 02 *****/
 $dataInicial = "2018-01-01";
 $dataFinal = "2018-02-01";
 $resultadoEsperado = 31;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("02", $resultadoEsperado, $resultado);
 
 /***** Teste 03 *****/
 $dataInicial = "2018-01-01";
 $dataFinal = "2018-02-02";
 $resultadoEsperado = 32;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("03", $resultadoEsperado, $resultado);
 
 /***** Teste 04 *****/
 $dataInicial = "2018-01-01";
 $dataFinal = "2018-02-28";
 $resultadoEsperado = 58;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("04", $resultadoEsperado, $resultado);
 
 /***** Teste 05 *****/
 $dataInicial = "2018-01-15";
 $dataFinal = "2018-03-15";
 $resultadoEsperado = 59;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("05", $resultadoEsperado, $resultado);
 
 /***** Teste 06 *****/
 $dataInicial = "2018-01-01";
 $dataFinal = "2019-01-01";
 $resultadoEsperado = 365;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("06", $resultadoEsperado, $resultado);
 
 /***** Teste 07 *****/
 $dataInicial = "2018-01-01";
 $dataFinal = "2020-01-01";
 $resultadoEsperado = 730;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("07", $resultadoEsperado, $resultado);
 
 /***** Teste 08 *****/
 $dataInicial = "2018-12-31";
 $dataFinal = "2019-01-01";
 $resultadoEsperado = 1;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("08", $resultadoEsperado, $resultado);
 
 /***** Teste 09 *****/
 $dataInicial = "2018-05-31";
 $dataFinal = "2018-06-01";
 $resultadoEsperado = 1;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("09", $resultadoEsperado, $resultado);
 
 /***** Teste 10 *****/
 $dataInicial = "2018-05-31";
 $dataFinal = "2019-06-01";
 $resultadoEsperado = 366;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("10", $resultadoEsperado, $resultado);
 
 /***** Teste 11 *****/
 $dataInicial = "2016-02-01";
 $dataFinal = "2016-03-01";
 $resultadoEsperado = 29;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("11", $resultadoEsperado, $resultado);
 
 /***** Teste 12 *****/
 $dataInicial = "2016-01-01";
 $dataFinal = "2016-03-01";
 $resultadoEsperado = 60;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("12", $resultadoEsperado, $resultado);
 
 /***** Teste 13 *****/
 $dataInicial = "1981-09-21";
 $dataFinal = "2009-02-12";
 $resultadoEsperado = 10006;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("13", $resultadoEsperado, $resultado);
 
 /***** Teste 14 *****/
 $dataInicial = "1981-07-31";
 $dataFinal = "2009-02-12";
 $resultadoEsperado = 10058;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("14", $resultadoEsperado, $resultado);
 
 /***** Teste 15 *****/
 $dataInicial = "2004-03-01";
 $dataFinal = "2009-02-12";
 $resultadoEsperado = 1809;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("15", $resultadoEsperado, $resultado);
 
 /***** Teste 16 *****/
 $dataInicial = "2004-03-01";
 $dataFinal = "2009-02-12";
 $resultadoEsperado = 1809;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("16", $resultadoEsperado, $resultado);
 
 /***** Teste 17 *****/
 $dataInicial = "1900-02-01";
 $dataFinal = "1900-03-01";
 $resultadoEsperado = 28;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("17", $resultadoEsperado, $resultado);
 
 /***** Teste 18 *****/
 $dataInicial = "1899-01-01";
 $dataFinal = "1901-01-01";
 $resultadoEsperado = 730;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("18", $resultadoEsperado, $resultado);
 
 /***** Teste 19 *****/
 $dataInicial = "2000-02-01";
 $dataFinal = "2000-03-01";
 $resultadoEsperado = 29;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("19", $resultadoEsperado, $resultado);
 
 /***** Teste 20 *****/
 $dataInicial = "1999-01-01";
 $dataFinal = "2001-01-01";
 $resultadoEsperado = 731;
-$resultado = $calculadora->calculaDias($dataInicial, $dataFinal);
+$resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("20", $resultadoEsperado, $resultado);
 
 
--- temperaturas.php
+++ temperaturas.php
@@ -1,70 +1,93 @@
 <?php
 
-require 'vendor/autoload.php';
-
-use Classes\CalculadoraTemperatura;
-
 /**
 * Retorna a temperatura mais proxima de zero.
 * Se duas temperaturas com o mesmo valor absouto (uma positiva e outra negativa) serem igualmente proxima a zero, deve ser dada a preferencia para o valor positivo.
 * @param array $temperaturas Lista de temperaturas
 * @return int A temperatura mais proxima de zero
 **/
+function menorTemperatura($temperaturas)
+{
 
-$calculadora = new CalculadoraTemperatura();
+    $maisProximoZero = null; // inicializa o maisProximoZero como null, pois pode ser que o mais proximo de 0 seja o proprio 0
+
+    foreach ($temperaturas as $temperatura) { // loop que percorre o array
+        if ($maisProximoZero === null) { // se for a primeira iteração, ele define o maisProximoZero como o primeiro numero do array
+            $maisProximoZero = $temperatura;
+            continue; // esse continue é para ele partir para o próximo ciclo no primeiro número
+        }
+
+        // essas duas linhas fazem a verificação de que se a temperatura for positiva, usa ela mesmo, e se não, transforma em positiva
+        $difAtual = $temperatura > 0 ? $temperatura : ($temperatura * -1); // diferença de 0 da temperatura atual
+        $difMenor = $maisProximoZero > 0 ? $maisProximoZero : ($maisProximoZero * -1); // diferença de 0 da temperatura que foi definida como menor até então
+
+        if ($difAtual < $difMenor) { // verifica se a diferença da temperatura atual for menor que a diferença da temperatura definida
+            $maisProximoZero = $temperatura;
+        } elseif ($difAtual === $difMenor && $temperatura > $maisProximoZero) { // verifica se as distâncias são iguais e coloca como prioridade a positiva
+            $maisProximoZero = $temperatura;
+        }
+    }
+
+    return $maisProximoZero;
+}
+
 
 /***** Teste 01 *****/
 $temperaturas = array( 17, 32, 14, 21 );
 $resultadoEsperado = 14;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("01", $resultadoEsperado, $resultado);
+
 
 /***** Teste 02 *****/
 $temperaturas = array( 27, -8, -12, 9 );
 $resultadoEsperado = -8;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("02", $resultadoEsperado, $resultado);
+
 
 /***** Teste 03 *****/
 $temperaturas = array( -6, 14, 42, 6, 25, -18 );
 $resultadoEsperado = 6;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("03", $resultadoEsperado, $resultado);
+
 
 /***** Teste 04 *****/
 $temperaturas = array( 34, 11, 13, -23, -11, 18 );
 $resultadoEsperado = 11;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("04", $resultadoEsperado, $resultado);
+
 
 /***** Teste 05 *****/
 $temperaturas = array( 17, 0, 28, -4 );
 $resultadoEsperado = 0;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("05", $resultadoEsperado, $resultado);
 
 /***** Teste 06 *****/
 $temperaturas = array( -10, 27, 9, -12 );
 $resultadoEsperado = 9;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("06", $resultadoEsperado, $resultado);
 
 /***** Teste 07 *****/
 $temperaturas = array( -47, -14, -5, -12, -8 );
 $resultadoEsperado = -5;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("07", $resultadoEsperado, $resultado);
 
 /***** Teste 08 *****/
 $temperaturas = array( -47, -14, -5, -12, -5 );
 $resultadoEsperado = -5;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("08", $resultadoEsperado, $resultado);
 
 /***** Teste 09 *****/
 $temperaturas = array( -7, 12, -13, 8 );
 $resultadoEsperado = -7;
-$resultado = $calculadora->menorTemperatura($temperaturas);
+$resultado = menorTemperatura($temperaturas);
 verificaResultado("09", $resultadoEsperado, $resultado);
 
 
